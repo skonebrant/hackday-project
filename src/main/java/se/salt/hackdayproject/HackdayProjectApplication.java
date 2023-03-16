7fb8b8a1a6e5c6fb5c1df9e0c3356eb63911033a
Skonebrant <carl-fredrik.skonebrant@appliedtechnology.se>
2023-03-16 12:49:36 +0100

    first commit

---
+++ src/main/java/se/salt/hackdayproject/HackdayProjectApplication.java
@@ -0,0 +1,13 @@
+package se.salt.hackdayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackdayProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackdayProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/salt/hackdayproject/HackdayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.salt.hackdayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackdayProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
